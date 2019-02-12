be7370a6ccf27400819fb77be64cfc8692e64bbc
yunz <zyun3939@gmail.com>
2019-02-11 22:34:50 -0800

    02_11

---
+++ src/leetcode/twoPointer/ThreeSum.java
@@ -0,0 +1,75 @@
+package leetcode.twoPointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k]);
+                    result.add(list);
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    k--;
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+            while (i < n - 2 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+        return result;
+    }
+    public List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n -2; i++) {
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    k--;
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+            while (i < n - 2 && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
+        return result;
+    }
+}
--- src/leetcode/union/NumberOfIslands2.java
+++ src/leetcode/union/NumberOfIslands2.java
@@ -6,33 +6,30 @@ import java.util.List;
 public class NumberOfIslands2 {
     public List<Integer> numIslands2(int m, int n, int[][] positions) {
         List<Integer> result = new ArrayList<>();
-        if (positions == null || positions.length == 0 ||
-                positions[0] == null || positions[0].length == 0 || m * n == 0) {
+        if (positions == null || positions.length == 0 || m <= 0 || n <= 0) {
             return result;
         }
-        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int[][] islands = new int[m][n];
         Union union = new Union(m * n);
         int count = 0;
-        int[][] islands = new int[m][n];
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
         for (int[] pos : positions) {
-            int row = pos[0], col = pos[1];
-            islands[row][col] = 1;
             count++;
+            int r = pos[0], c = pos[1];
+            islands[r][c] = 1;
             for (int[] dir : dirs) {
-                int nRow = row + dir[0];
-                int nCol = col + dir[1];
-                if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && islands[nRow][nCol] == 1) {
-                    int oldFather = union.find(row * n + col);
-                    int newFather = union.find(nRow * n + nCol);
-                    if (oldFather != newFather) {
-                        union.union(oldFather, newFather);
+                int nRow = r + dir[0], nCol = c + dir[1];
+                if (nRow >= 0 && nCol >= 0 && nRow < m && nCol < n && islands[nRow][nCol] == 1) {
+                    int curFather = union.find(r * n + c);
+                    int oldFather = union.find(nRow * n + nCol);
+                    if (curFather != oldFather) {
                         count--;
+                        union.union(r * n + c, nRow * n + nCol);
                     }
                 }
             }
             result.add(count);
         }
-
         return result;
     }
 }
