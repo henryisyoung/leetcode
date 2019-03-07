2504113aae88344619014e3af77de988af1e6e64
yunz <zyun3939@gmail.com>
2019-03-07 14:08:04 -0800

    03_07

--- src/airbnb/KEditDistance.java
+++ src/airbnb/KEditDistance.java
@@ -103,13 +103,18 @@ public class KEditDistance {
     public boolean isValidPair(String word, String target, int k) {
         int m = word.length(), n = target.length();
         int[][] dp = new int[m + 1][n + 1];
-
+        for (int i = 1; i<= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (word.charAt(i - 1) == target.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
                 }
             }
         }
@@ -118,7 +123,7 @@ public class KEditDistance {
 
     public static void main(String[] args) {
         KEditDistance editor = new KEditDistance();
-        String[] words = {"abc", "abd", "abcd", "adc","abcde","a"};
+        String[] words = {"abc", "abd", "abcd", "adc","abcde","a", "gkl"};
         String target = "ac";
         int k = 2;
         List<String> list = editor.getKEditDistanceDP(words, target, k);
--- src/airbnb/MenuCombinationSum.java
+++ src/airbnb/MenuCombinationSum.java
@@ -5,38 +5,39 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MenuCombinationSum {
-    private static void search(List<List<Double>> res, int[] centsPrices, int start, int centsTarget,
-                               List<Double> curCombo, double[] prices) {
-        if (centsTarget == 0) {
-            res.add(new ArrayList<>(curCombo));
-            return;
+    public static List<List<Double>> getCombos(double[] prices, double target) {
+        List<List<Double>> result = new ArrayList<>();
+        if (prices == null || prices.length == 0) {
+            return result;
         }
-        for (int i = start; i < centsPrices.length; i++) {
-            if (i > start && centsPrices[i] == centsPrices[i - 1]) {
-                continue;
-            }
-            if (centsPrices[i] > centsTarget) {
-                break;
-            }
-            curCombo.add(prices[i]);
-            search(res, centsPrices, i + 1, centsTarget - centsPrices[i], curCombo, prices);
-            curCombo.remove(curCombo.size() - 1);
+        int n = prices.length;
+        Arrays.sort(prices);
+        int roundtarget = (int) Math.round(target * 100);
+        int[] roundPrices = new int[n];
+        for (int i = 0; i < n; i++) {
+            roundPrices[i] = (int) Math.round(100 * prices[i]);
         }
+        search(result, roundPrices, roundtarget, prices, new ArrayList<Double>(), 0, 0);
+        return result;
     }
 
-    public static List<List<Double>> getCombos(double[] prices, double target) {
-        List<List<Double>> res = new ArrayList<>();
-        if (prices == null || prices.length == 0 || target <= 0) {
-            return res;
+    private static void search(List<List<Double>> result, int[] roundPrices, int roundtarget, double[] prices,
+                               ArrayList<Double> list, int pos, int sum) {
+        if (sum == roundtarget) {
+            result.add(new ArrayList<>(list));
+            return;
         }
-        int centsTarget = (int) Math.round(target * 100);
-        Arrays.sort(prices);
-        int[] centsPrices = new int[prices.length];
-        for (int i = 0; i < prices.length; i++) {
-            centsPrices[i] = (int) Math.round(prices[i] * 100);
+        for (int i = pos; i < prices.length; i++) {
+            if (i != pos && prices[i] == prices[i - 1]) {
+                continue;
+            }
+            if (sum + roundPrices[i] > roundtarget) {
+                break;
+            }
+            list.add(prices[i]);
+            search(result, roundPrices, roundtarget, prices, list, i + 1, sum + roundPrices[i]);
+            list.remove(list.size() - 1);
         }
-        search(res, centsPrices, 0, centsTarget, new ArrayList<Double>(), prices);
-        return res;
     }
 
     public static List<List<Double>> getCombos2(double[] prices, double target) {
@@ -76,7 +77,7 @@ public class MenuCombinationSum {
 
     public static void main(String[] args) {
         double[] prices = {2.40, 0.01, 6.00, 2.58};
-        List<List<Double>> result = getCombos2(prices, 8.59);
+        List<List<Double>> result = getCombos(prices, 8.59);
         for (int i = 0; i < result.size(); i++) {
             System.out.println(i + "th result:");
             for (int j = 0; j < result.get(i).size(); j++) {
