0b6ff8aa4cd653549120466408143a63b43a2323
Yun Zou <yunzo@microsoft.com>
2019-03-21 16:28:23 -0700

    03_21

--- src/airbnb/CheapestFlightsWithinKStopsBFS.java
+++ src/airbnb/CheapestFlightsWithinKStopsBFS.java
@@ -4,41 +4,49 @@ import java.util.*;
 
 public class CheapestFlightsWithinKStopsBFS {
     public int findCheapestPriceBFS(int n, int[][] flights, int src, int dst, int K) {
-        if (flights == null || flights.length == 0 || flights[0] == null || flights[0].length == 0) {
+        if (flights == null || flights.length == 0 ||
+                flights[0] == null || flights[0].length == 0) {
             return -1;
         }
-        int result = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
+
         Map<Integer, List<Place>> map = new HashMap<>();
         for (int[] flight : flights) {
-            int from = flight[0], to = flight[1], cost = flight[2];
-            if (!map.containsKey(from)) {
-                map.put(from, new ArrayList<Place>());
+            int s = flight[0], d = flight[1], c = flight[2];
+            Place p = new Place(d, c);
+            if (map.containsKey(s)) {
+                List<Place> list = map.get(s);
+                list.add(p);
+                map.put(s, list);
+            } else {
+                List<Place> list = new ArrayList<>();
+                list.add(p);
+                map.put(s, list);
             }
-            map.get(from).add(new Place(to, cost));
         }
 
-        Queue<Place> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n];
-
-        visited[src] = true;
-        queue.add(new Place(src, 0));
+        Queue<Place> pq = new LinkedList<>();
+        pq.add(new Place(src, 0));
+        Set<Integer> isVisited = new HashSet<>();
+        isVisited.add(src);
         int count = 0;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
+        while (!pq.isEmpty()) {
+            int size = pq.size();
             for (int i = 0; i < size; i++) {
-                Place cur = queue.poll();
-                int curPlace = cur.place;
-                if (cur.place == dst) {
-                    result = Math.min(cur.cost, result);
+                Place cur = pq.poll();
+                int from = cur.des;
+                int fee = cur.cost;
+                if (from == dst) {
+                    ans = Math.min(ans, fee);
                 }
-                if (map.containsKey(curPlace)) {
-                    for (Place nextPlace : map.get(curPlace)) {
-                        if (visited[nextPlace.place] && nextPlace.cost > result) {
+                List<Place> nexts = map.get(from);
+                if (nexts != null) {
+                    for (Place next : nexts) {
+                        if (next.cost + fee > ans) {
                             continue;
                         }
-                        queue.add(new Place(nextPlace.place, cur.cost + nextPlace.cost));
-                        visited[nextPlace.place] = true;
+                        pq.add(new Place(next.des, next.cost + fee));
+                        isVisited.add(next.des);
                     }
                 }
             }
@@ -47,16 +55,21 @@ public class CheapestFlightsWithinKStopsBFS {
             }
             count++;
         }
-
-        return result == Integer.MAX_VALUE ? -1 : result;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
+
     private class Place {
-        int place;
+        int des;
         int cost;
         public Place (int des, int cost) {
-            this.place = des;
+            this.des = des;
             this.cost = cost;
+        }
+
+        @Override
+        public String toString() {
+            return "des:" + des + " cost:" + cost;
         }
     }
 
