8ce26ecf498fc0074a60c92eb7d00776507a1bd3
Yun Zou <yunzo@microsoft.com>
2019-03-25 15:43:44 -0700

    03_25

--- src/leetcode/NestedIterator.java
+++ src/leetcode/NestedIterator.java
@@ -1,36 +1,36 @@
-//package leetcode;
-//import java.util.*;
-//public class NestedIterator implements Iterator<Integer> {
-//    Stack<NestedInteger> stack = new Stack<NestedInteger>();;
-//    public NestedIterator(List<NestedInteger> nestedList) {
-//        if(nestedList == null){
-//        	return;
-//        }
-//        for(int i = nestedList.size() - 1; i >= 0; i--){
-//        	stack.push(nestedList.get(i));
-//        }
-//    }
-//
-//
-//
-//	@Override
-//    public Integer next() {
-//        return stack.pop().getInteger();
-//    }
-//
-//    @Override
-//    public boolean hasNext() {
-//    	while(!stack.isEmpty()){
-//        	NestedInteger n = stack.peek();
-//        	if(n.isInteger()){
-//        		return true;
-//        	}else{
-//        		stack.pop();
-//        		for(int i = n.getList().size() - 1; i >= 0; i--){
-//        			stack.push(n.getList().get(i));
-//        		}
-//        	}
-//    	}
-//    	return false;
-//    }
-//}
+package leetcode;
+import java.util.*;
+public class NestedIterator implements Iterator<Integer> {
+    Stack<NestedInteger> stack = new Stack<NestedInteger>();;
+    public NestedIterator(List<NestedInteger> nestedList) {
+        if (nestedList.size() == 0 || nestedList == null) {
+            return;
+        }
+        int n = nestedList.size();
+        for (int i = n - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+
+	@Override
+    public Integer next() {
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+    	while (!stack.isEmpty()) {
+    	    NestedInteger cur = stack.peek();
+    	    if (cur.isInteger()) {
+    	        return true;
+            } else {
+    	        stack.pop();
+                List<NestedInteger> list = cur.getList();
+    	        for (int i = list.size() - 1; i >= 0; i--) {
+    	            stack.push(list.get(i));
+                }
+            }
+        }
+        return false;
+    }
+}
