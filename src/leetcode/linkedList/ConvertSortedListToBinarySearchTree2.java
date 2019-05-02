8555c600ee7ed2a96e502b02b282fe9237756f29
yunz <zyun3939@gmail.com>
2019-05-01 21:46:41 -0700

    05_01

---
+++ src/leetcode/linkedList/ConvertSortedListToBinarySearchTree2.java
@@ -0,0 +1,36 @@
+package leetcode.linkedList;
+
+import Bloomberg.TreeNode;
+
+public class ConvertSortedListToBinarySearchTree2 {
+    ListNode node;
+    public TreeNode sortedListToBST(ListNode head) {
+        this.node = head;
+        int size = findSize(head);
+        return sortListHelper(0, size - 1);
+    }
+
+    private TreeNode sortListHelper(int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode left = sortListHelper(start, mid - 1);
+        TreeNode root = new TreeNode(node.val);
+        root.left = left;
+        node = node.next;
+        TreeNode right = sortListHelper(mid + 1, end);
+        root.right = right;
+        return root;
+    }
+
+    private int findSize(ListNode head) {
+        ListNode n = head;
+        int count = 0;
+        while (n != null) {
+            n = n.next;
+            count++;
+        }
+        return count;
+    }
+}
---
+++ src/leetcode/linkedList/SortList.java
@@ -0,0 +1,46 @@
+package leetcode.linkedList;
+
+public class SortList {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = findMid(head);
+        ListNode list2 = sortList(mid.next);;
+        mid.next = null;
+        ListNode list1 = sortList(head);
+
+        return mergeTwoList(list1, list2);
+    }
+
+    private ListNode mergeTwoList(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if (list1 != null) {
+            cur.next = list1;
+        }
+        if (list2 != null) {
+            cur.next = list2;
+        }
+        return dummy.next;
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
