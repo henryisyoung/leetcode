7b96b651ce49257b39480a87abc59dd15d4dd089
yunz <zyun3939@gmail.com>
2019-05-08 22:16:24 -0700

    05_08

---
+++ src/pinterest/MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,21 @@
+package pinterest;
+
+public class MaximumLengthOfRepeatedSubarray {
+    public int findLength(int[] A, int[] B) {
+        if (A == null || B == null || A.length == 0 || B.length == 0) {
+            return 0;
+        }
+        int m = A.length, n = B.length;
+        int result  = 0;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                result = Math.max(result, dp[i][j]);
+            }
+        }
+        return result;
+    }
+}
