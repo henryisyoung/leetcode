75a96c97946395d25031609fd57b2cecf51decf7
Yun Zou <yunzo@microsoft.com>
2019-08-17 12:01:13 -0700

    08_16

--- src/design/taskScheduler/TaskScheduler.java
+++ src/design/taskScheduler/TaskScheduler.java
@@ -5,7 +5,7 @@ import java.util.concurrent.DelayQueue;
 public class TaskScheduler {
     public static void main(String[] args) {
         DelayQueue<Task> queue = new DelayQueue<>();
-        new Thread(new TaskProducer(queue), "Producer Thread").start();
         new Thread(new TaskConsumer(queue), "Consumer Thread").start();
+        new Thread(new TaskProducer(queue), "Producer Thread").start();
     }
 }
--- src/pinterest/HitCounter.java
+++ src/pinterest/HitCounter.java
@@ -1,35 +1,79 @@
 package pinterest;
 
+import java.util.*;
+import java.util.concurrent.*;
+
 public class HitCounter {
-    int[] timestamps, hits;
+    Map<Integer, Node> map;
     /** Initialize your data structure here. */
     public HitCounter() {
-        this.hits = new int[300];
-        this.timestamps = new int[300];
-    }
-
-    /** Record a hit.
-     @param timestamp - The current timestamp (in seconds granularity). */
-    public void hit(int timestamp) {
-        int index = timestamp % 300;
-        if (timestamps[index] != timestamp) {
-            timestamps[index] = timestamp;
-            hits[index] = 1;
-        } else {
-            hits[index]++;
+       this.map = new ConcurrentHashMap<>();
+        for (int i = 0; i < 300; i++) {
+            map.put(i, new Node(0, 0));
         }
     }
 
-    /** Return the number of hits in the past 5 minutes.
-     @param timestamp - The current timestamp (in seconds granularity). */
-    public int getHits(int timestamp) {
+    public synchronized void hit() {
+        //System.out.println("Thread hits " + Thread.currentThread().getName());
+        long curTime = System.currentTimeMillis() / 1000;
+        int index = (int) (curTime % 300);
+        if (map.get(index).timestamp == curTime) {
+            map.get(index).count++;
+        } else {
+            map.get(index).count = 1;
+            map.get(index).timestamp = curTime;
+        }
+    }
+
+    public synchronized int getHits() {
+        long curTime = System.currentTimeMillis() / 1000;
         int count = 0;
         for (int i = 0; i < 300; i++) {
-            int time = timestamps[i];
-            if (timestamp - time < 300) {
-                count += hits[i];
+            if (map.get(i).timestamp + 300 > curTime) {
+                count += map.get(i).count;
             }
         }
         return count;
+    }
+
+    class Node{
+        int count;
+        long timestamp;
+        public Node(int count, long timestamp) {
+            this.count = count;
+            this.timestamp = timestamp;
+        }
+    }
+
+    public static void main(String[] args) {
+        HitCounter hitter = new HitCounter();
+        for (int t = 0; t < 10; t++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            hitter.hit();
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }, "thread-" + t).start();
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        System.out.println("now we have hits " + hitter.getHits());
+                        Thread.sleep(1500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "thread-get" ).start();
     }
 }
